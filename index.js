1149e42c53a2b5cf46b1365ef595b4a138abad38
Omar Betar <omar1betar@gmail.com>
2023-03-20 11:21:43 +0200

    addCustomersData

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ app.listen(port, () => console.log(`app is started on port ${port}`));
 
 app.use('/', (req, res, next) => {
     if (req.path === '/') {
-      
+
         res.status(200).send({ 'test': true });
     } else {
-        
+
         next();
   }
 });
 
 app.use('/data', (req, res, next) => {
-    
+
     res.status(200).send(
         {
         "data": {
@@ -53,4 +53,64 @@ app.use('/data', (req, res, next) => {
         }
         }
     );
+});
+
+app.use('/customers', (req, res, next) => {
+
+  res.status(200).send(
+    {
+      "customers": [
+        {
+          "id": "1",
+          "name": "John Doe",
+          "email": "johndoe@email.com",
+          "phone": "555-123-4567",
+          "address": "123 Main St",
+          "city": "Anytown",
+          "state": "CA",
+          "zip": "12345"
+        },
+        {
+          "id": "2",
+          "name": "Jane Smith",
+          "email": "janesmith@email.com",
+          "phone": "555-234-5678",
+          "address": "456 Oak St",
+          "city": "Sometown",
+          "state": "NY",
+          "zip": "67890"
+        },
+        {
+          "id": "3",
+          "name": "Bob Johnson",
+          "email": "bob.johnson@email.com",
+          "phone": "555-345-6789",
+          "address": "789 Elm St",
+          "city": "Anyville",
+          "state": "TX",
+          "zip": "54321"
+        },
+        {
+          "id": "4",
+          "name": "Sarah Lee",
+          "email": "sarah.lee@email.com",
+          "phone": "555-456-7890",
+          "address": "321 Pine St",
+          "city": "Sometown",
+          "state": "CA",
+          "zip": "09876"
+        },
+        {
+          "id": "5",
+          "name": "Mark Jones",
+          "email": "mark.jones@email.com",
+          "phone": "555-567-8901",
+          "address": "654 Maple St",
+          "city": "Anytown",
+          "state": "NY",
+          "zip": "23456"
+        }
+      ]
+    }
+  );
 });
